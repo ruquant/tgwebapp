41b631aac0d3d4079b37eb8d27b930b566c2ccfd
ruquant <dmitriy.kovalevskiy@gmail.com>
2023-04-02 23:42:48 +0300

    updated with info from gpt4

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// Include the Matter.js library at the beginning of your app.js file
-import { Engine, Render, World, Bodies, Body, Mouse, MouseConstraint } from 'https://cdn.skypack.dev/matter-js';
+import { Engine, Render, World, Bodies, Mouse, MouseConstraint } from 'https://cdn.skypack.dev/matter-js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const { WebApp } = window.Telegram;
@@ -10,19 +9,23 @@ document.addEventListener('DOMContentLoaded', () => {
   // Create game engine and canvas renderer
   const engine = Engine.create();
   const canvas = document.getElementById('gameCanvas');
-  const context = canvas.getContext('2d');
-  const render = {
+  const render = Render.create({
     canvas,
-    context,
     engine,
-  };
+    options: {
+      width: canvas.width,
+      height: canvas.height,
+      wireframes: false,
+      background: '#f0f0f0',
+    },
+  });
 
   // Create game objects
-  const ground = Bodies.rectangle(400, 590, 810, 20, { isStatic: true });
-  const bird = Bodies.circle(150, 300, 20);
-  const block1 = Bodies.rectangle(600, 550, 50, 50);
-  const block2 = Bodies.rectangle(650, 550, 50, 50);
-  const block3 = Bodies.rectangle(625, 500, 100, 20);
+  const ground = Bodies.rectangle(400, 590, 810, 20, { isStatic: true, render: { fillStyle: '#888' } });
+  const bird = Bodies.circle(150, 300, 20, { render: { fillStyle: '#f00' } });
+  const block1 = Bodies.rectangle(600, 550, 50, 50, { render: { fillStyle: '#888' } });
+  const block2 = Bodies.rectangle(650, 550, 50, 50, { render: { fillStyle: '#888' } });
+  const block3 = Bodies.rectangle(625, 500, 100, 20, { render: { fillStyle: '#888' } });
 
   // Add objects to the world
   World.add(engine.world, [ground, bird, block1, block2, block3]);
@@ -35,53 +38,9 @@ document.addEventListener('DOMContentLoaded', () => {
   // Game loop
   const gameLoop = () => {
     Engine.update(engine, 1000 / 60);
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Draw game objects
-    drawRectangle(ground, context);
-    drawCircle(bird, context);
-    drawRectangle(block1, context);
-    drawRectangle(block2, context);
-    drawRectangle(block3, context);
-
-    Engine.update(engine, 1000 / 60);
     Render.world(render);
-    requestAnimationFrame(gameLoop);
-
     requestAnimationFrame(gameLoop);
   };
 
   gameLoop();
-
-  function drawRectangle(body, ctx) {
-    const { position, angle, vertices } = body;
-    ctx.save();
-    ctx.translate(position.x, position.y);
-    ctx.rotate(angle);
-    ctx.beginPath();
-    ctx.moveTo(vertices[0].x - position.x, vertices[0].y - position.y);
-    for (let i = 1; i < vertices.length; i++) {
-      ctx.lineTo(vertices[i].x - position.x, vertices[i].y - position.y);
-    }
-    ctx.closePath();
-    ctx.fillStyle = '#888';
-    ctx.fill();
-    ctx.strokeStyle = '#000';
-    ctx.stroke();
-    ctx.restore();
-  }
-
-  function drawCircle(body, ctx) {
-    const { position, angle, circleRadius } = body;
-    ctx.save();
-    ctx.translate(position.x, position.y);
-    ctx.rotate(angle);
-    ctx.beginPath();
-    ctx.arc(0, 0, circleRadius, 0, 2 * Math.PI);
-    ctx.fillStyle = '#f00';
-    ctx.fill();
-    ctx.strokeStyle = '#000';
-    ctx.stroke();
-    ctx.restore();
-  }
 });
